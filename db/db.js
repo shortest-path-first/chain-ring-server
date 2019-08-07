5de3dd32054814f716c294d94f470ec9ab15c071
semaj <semajbarthelemy55@gmail.com>
2019-08-06 19:30:13 -0500

    add template database functions

--- db/db.js
+++ db/db.js
@@ -1,3 +1,36 @@
-const models = require('./index');
+const {Marker, Location, User, Ride, Route, Stat} = require('./index');
 
 console.log(models.User);
+
+const addMarker = () => {
+  Marker.build({}).save()
+};
+
+const addLocation = () => {
+  Location.build({}).save()
+};
+
+const addUser = () => {
+  User.build({}).save()
+};
+
+const addRide = () => {
+  Ride.build({}).save()
+};
+
+const addRoute = () => {
+  Route.build({}).save()
+};
+
+const addStat = () => {
+  Stat.build({}).save()
+};
+
+module.exports = {
+  addLocation,
+  addMarker,
+  addRide,
+  addRoute,
+  addStat,
+  addUser,
+}
