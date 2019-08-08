af7c67ef63283b25e5854efefc4d3da1cd8fcb7b
semaj <semajbarthelemy55@gmail.com>
2019-08-08 17:08:03 -0500

    add get user request handler and template request handlers

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+app.get('/user', (req, res) => {
+  const { username } = req.body;
+  db.getUser(username)
+    .then((users) => {
+      res.send(users[0]);
+    });
+});
 app.post('/user', (req, res) => {
   console.log(req.body);
   const { username } = req.body;
@@ -17,6 +24,56 @@ app.post('/user', (req, res) => {
 app.patch('/user', (req, res) => {
   const { name, speed, distance } = req.body;
   db.updateUser({ name, speed, distance }, res);
+});
+
+app.get('/marker', (req, res) => {
+  res.end();
+});
+app.post('/marker', (req, res) => {
+  res.end();
+});
+app.patch('/marker', (req, res) => {
+  res.end();
+});
+
+app.get('/location', (req, res) => {
+  res.end();
+});
+app.post('/location', (req, res) => {
+  res.end();
+});
+app.patch('/location', (req, res) => {
+  res.end();
+});
+
+app.get('/stat', (req, res) => {
+  res.end();
+});
+app.post('/stat', (req, res) => {
+  res.end();
+});
+app.patch('/stat', (req, res) => {
+  res.end();
+});
+
+app.get('/route', (req, res) => {
+  res.end();
+});
+app.post('/route', (req, res) => {
+  res.end();
+});
+app.patch('/route', (req, res) => {
+  res.end();
+});
+
+app.get('/ride', (req, res) => {
+  res.end();
+});
+app.post('/ride', (req, res) => {
+  res.end();
+});
+app.patch('/ride', (req, res) => {
+  res.end();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
